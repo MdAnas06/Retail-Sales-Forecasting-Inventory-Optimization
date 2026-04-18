15f9dffc4839436598aecb736294f8b824580ee8
MdAnas06 <91mdanas@gmail.com>
2026-04-18 23:22:15 +0530

    Step 2: Data preprocessing and validation pipeline added

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ df = pd.read_csv("data/raw/sales.csv")
 
 print(df.head())
 print("\nDataset Shape:", df.shape)
+from src.preprocessing import preprocess_data
+
+df = preprocess_data("data/raw/sales.csv")
+
+print("\nCleaned Data Preview:\n")
+print(df.head())
+
+# Save processed data
+df.to_csv("data/processed/clean_sales.csv", index=False)
+
+print("\nProcessed data saved successfully!")
